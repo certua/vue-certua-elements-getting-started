53845c69fe5d1e0a57dbedcb76fefe665aca9ae8
charleystagg-certua <charley@certua.io>
2024-01-31 12:55:17 +0000

    fix: update routes

--- src/components/insurance/QuoteAndBuy.vue
+++ src/components/insurance/QuoteAndBuy.vue
@@ -25,16 +25,16 @@ onMounted(() => {
 })
 addEventListener('saveQuote', (event: any) => {
   console.log('save quote received')
-  window.location.href = '/angular/components/login'
+  window.location.href = 'vue/insurance/components/login'
 })
 window.addEventListener('signup', (event: any) => {
   console.log('sign up event received')
 
   if (!event.detail.autoSignUp) {
-    window.location.href = '/angular/components/login'
+    window.location.href = 'vue/insurance/components/login'
   } else {
     console.log('dispatch success')
-    window.location.href = '/angular/components/login'
+    window.location.href = 'vue/insurance/components/login'
   }
 })
 </script>
